f2e4c8046a3d23487bb014aa92b1d32e7f6de4fb
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-02-03 10:01:08 -0500

    nice

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
         <p style={{ color: "var(--tone20)" }}>find talents and connect with creative people</p>
 
         <div style={{ display: "grid", gridTemplateColumns: "auto 1fr", marginTop: "1rem", width: "80%", maxWidth: "600px", borderRadius: "1rem", overflow: "clip", border: '2px solid var(--tone95)' }}>
-          <div style={{ padding: ".5rem", backgroundColor: "var(--tone95)", display: "grid", alignItems: 'center', justifyItems: "center" }}>
+          <div style={{ padding: "1rem", backgroundColor: "var(--tone95)", display: "grid", alignItems: 'center', justifyItems: "center" }}>
             <svg style={{ width: "1.5rem" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
           </div>
 
-          <input type="text" placeholder="Filter by name" />
+          <input style={{ borderTopRightRadius: "1rem", borderBottomRightRadius: "1rem" }} type="text" placeholder="Filter by name" />
         </div>
 
         <div style={{ display: 'flex', flexWrap: "wrap", gap: "1rem", alignItems: "center" }}>
